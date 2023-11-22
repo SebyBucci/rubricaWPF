47ba148d7196f67ea6c14dda7bbc03f4b6c917d3
Sebastian Bucci <127589946+SebyBucci@users.noreply.github.com>
2023-11-22 08:55:39 +0100

    Add files via upload

---
+++ bucci.sebastian.4i.rubrica/bucci.sebastian.4i.rubrica/Contatto.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace bucci.sebastian._4i.rubrica
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    internal class Contatto
+    {
+        private int numero;
+        private string nome;
+        private string cognome;
+
+        public int Numero { get => numero; set => numero = value; }
+        public string Nome { get => nome; set => nome = value; }
+        public string Cognome { get => cognome; set => cognome = value; }
+    }
+}
