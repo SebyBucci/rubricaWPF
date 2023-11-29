3cecf2d7af3c192f17b0d6294b8959cbf7687259
Sebastian Bucci <127589946+SebyBucci@users.noreply.github.com>
2023-11-29 09:15:58 +0100

    Add files via upload

---
+++ bucci.sebastian.4i.rubricaColor/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace bucci.sebastian._4i.rubrica
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
